5b2a50d542f294fe42d8b2676ee9363f0d8df520
rishesh007 <risheshagarwala1@gmail.com>
2019-09-02 02:10:49 +0530

    index main app file

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var inMode = require('./controllers/inMode');
+var app = express();
+
+//set up engine
+app.set('view engine', 'ejs');
+
+//use static files
+app.use(express.static('.public'));
+
+//fire InMode controller
+inMode(app);
+
+app.listen(3403);
